5f2692bf6ba9e93f4d75b45694ea5619457c8a9a
OlegSokol <soklakov.o.yu@gmail.com>
2017-03-28 21:14:48 +0300

    fix tests

--- src/test/java/ua/jscript_runner/AllTests.java
+++ src/test/java/ua/jscript_runner/AllTests.java
@@ -6,12 +6,11 @@ import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import ua.jscript_runner.controller.ScriptControllerTest;
 import ua.jscript_runner.service.ScriptServiceTest;
-import ua.jscript_runner.thread.executor.ScriptExecutorConcurrentHandlerTest;
 import ua.jscript_runner.util.EngineManagerTest;
 
 @RunWith(Suite.class)
 @SpringBootTest(classes = Application.class)
-@Suite.SuiteClasses({ ScriptControllerTest.class, ScriptServiceTest.class, EngineManagerTest.class, ScriptExecutorConcurrentHandlerTest.class} )
+@Suite.SuiteClasses({ ScriptControllerTest.class, ScriptServiceTest.class, EngineManagerTest.class} )
 public class AllTests {
     @Test
     public void mockTest() {
